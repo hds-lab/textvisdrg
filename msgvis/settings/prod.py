b85ba3ca51eae873566046357e1f13af4e868526
Michael Brooks <mjbrooks@uw.edu>
2015-02-08 03:23:15 -0800

    hopefully allowed hosts working

--- msgvis/settings/prod.py
+++ msgvis/settings/prod.py
@@ -8,4 +8,4 @@ DEBUG_TEMPLATE = DEBUG
 COMPRESS_ENABLED = bool(get_env_setting('COMPRESS_ENABLED', True))
 COMPRESS_OFFLINE = bool(get_env_setting('COMPRESS_OFFLINE', True))
 
-ALLOWED_HOSTS = ('127.0.0.1','localhost',)
+ALLOWED_HOSTS = get_env_setting("ALLOWED_HOSTS", 'localhost').split(',')
