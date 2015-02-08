938063cda5e36f522cc591067a25d12f856b9da1
Michael Brooks <mjbrooks@uw.edu>
2015-02-08 01:53:20 -0800

    full path to base app

--- msgvis/settings/common.py
+++ msgvis/settings/common.py
@@ -207,7 +207,7 @@ DJANGO_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
-    'base',
+    'msgvis.base',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
