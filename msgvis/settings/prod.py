8f9d1a83817ca4a9e978f9a2586db4f38f4a8fdd
Michael Brooks <mjbrooks@uw.edu>
2015-02-08 02:36:27 -0800

    more settings

--- msgvis/settings/prod.py
+++ msgvis/settings/prod.py
@@ -1,9 +1,14 @@
 from msgvis.settings.common import *
 
-DEBUG = False
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-DEBUG_TEMPLATE = False
+DEV = False
+
+DEBUG = bool(get_env_setting('DEBUG', False))
+DEBUG_TEMPLATE = DEBUG
+
+COMPRESS_ENABLED = bool(get_env_setting('COMPRESS_ENABLED', True))
+COMPRESS_OFFLINE = bool(get_env_setting('COMPRESS_OFFLINE', True))
+
 
 ALLOWED_HOSTS = ('127.0.0.1','localhost',)
 
+print DEBUG
