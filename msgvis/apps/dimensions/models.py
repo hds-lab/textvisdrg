21a12a10bbf363d2666ed12f541440907e7575f5
Nan-Chen Chen <nanchen.chen@gmail.com>
2015-02-15 12:04:46 -0800

    adding filter functions to the dimensions

--- msgvis/apps/dimensions/models.py
+++ msgvis/apps/dimensions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Q
+import operator
 from msgvis.apps.corpus import models as corpus_models
 from msgvis.apps.dimensions import distributions
 
@@ -32,20 +34,52 @@ class BaseDimension(object):
 
         return self.distribution.group_by(dataset, self.field_name)
 
+    def exact_filter(self, dataset, filter):
+        """Filtering for exact value"""
+        if filter.get('value'):
+            dataset = dataset.filter(Q((self.field_name, filter['value'])))
+        return dataset
+
+    def filter(self, dataset, filter):
+        return self.exact_filter(dataset, filter)
+
 
 class QuantitativeDimension(BaseDimension):
     """A generic quantitative dimension"""
     distribution = distributions.QuantitativeDistribution()
 
+    def filter(self, dataset, filter):
+        dataset = self.exact_filter(dataset, filter)
+        if filter.get('min'):
+            dataset = dataset.filter(Q((self.field_name + "__gte", filter['min'])))
+        if filter.get('max'):
+            dataset = dataset.filter(Q((self.field_name + "__lte", filter['max'])))
+        return dataset
 
 class TimeDimension(QuantitativeDimension):
     """A dimension for time variables"""
     distribution = distributions.TimeDistribution()
 
+    def filter(self, dataset, filter):
+        dataset = self.exact_filter(dataset, filter)
+        if filter.get('min_time'):
+            dataset = dataset.filter(Q((self.field_name + "__gte", filter['min_time'])))
+        if filter.get('max_time'):
+            dataset = dataset.filter(Q((self.field_name + "__lte", filter['max_time'])))
+        return dataset
 
 class CategoricalDimension(BaseDimension):
     """A generic categorical dimension"""
     distribution = distributions.CategoricalDistribution()
+
+    def filter(self, dataset, filter):
+        dataset = self.exact_filter(dataset, filter)
+        if filter.get('levels'):
+            filter_ors = []
+            for level in filter.get('levels'):
+                filter_ors.append((self.field_name, level))
+            dataset = dataset.filter(reduce(operator.or_, [Q(x) for x in filter_ors]))
+        return dataset
 
 
 class ForeignKeyDimension(CategoricalDimension):
