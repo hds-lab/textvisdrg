83ebbde3e52b551d1698df06c39d5c63336aca70
Michael Brooks <mjbrooks@uw.edu>
2015-02-16 17:54:31 -0800

    datatables working for categorical dimensions

--- msgvis/apps/dimensions/models.py
+++ msgvis/apps/dimensions/models.py
@@ -7,6 +7,7 @@ from django.db.models import query
 from django.db.models import Q
 from django.conf import settings
 
+from msgvis.apps.base.models import MappedValuesQuerySet
 from msgvis.apps.corpus import models as corpus_models
 
 
@@ -30,51 +31,6 @@ def find_messages(queryset):
     if isinstance(queryset, corpus_models.Dataset):
         queryset = queryset.message_set.all()
     return queryset
-
-
-class MappedValuesQuerySet(query.ValuesQuerySet):
-    """
-    A special ValuesQuerySet that can re-map the dictionary keys
-    while they are bing iterated over.
-
-    .. code-block:: python
-
-        valuesQuerySet = queryset.values('some__ugly__field__expression')
-        mapped = MappedQuerySet.create_from(valuesQuerySet, {
-            'some__ugly__field__expression': 'nice_expression'
-        })
-        mapped[0]
-        # { 'nice_expression': 5 }
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(MappedValuesQuerySet, self).__init__(*args, **kwargs)
-        self.field_map = kwargs.get('field_map', {})
-
-    @classmethod
-    def create_from(cls, values_query_set, field_map):
-        """Create a MappedValueQuerySet with a field name mapping dictionary."""
-        return values_query_set._clone(cls, field_map=field_map)
-
-    def _clone(self, klass=None, setup=False, **kwargs):
-        c = super(MappedValuesQuerySet, self)._clone(klass, setup, **kwargs)
-        c.field_map = self.field_map
-        return c
-
-    def iterator(self):
-        # Purge any extra columns that haven't been explicitly asked for
-        extra_names = list(self.query.extra_select)
-        field_names = self.field_names
-        aggregate_names = list(self.query.aggregate_select)
-
-        names = extra_names + field_names + aggregate_names
-
-        # Remap the fields, but fall back on regular name
-        names = [self.field_map.get(name, name) for name in names]
-
-        for row in self.query.get_compiler(self.db).results_iter():
-            yield dict(zip(names, row))
 
 
 class CategoricalDimension(object):
