751c693570c61b15f51e1fdb1ef68b09921338e1
Michael Brooks <mjbrooks@uw.edu>
2015-03-10 12:41:39 -0700

    remove memory leak in import for #124

--- msgvis/apps/importer/management/commands/import_corpus.py
+++ msgvis/apps/importer/management/commands/import_corpus.py
@@ -7,6 +7,8 @@ from django.db import transaction
 import traceback
 import sys
 import path
+from time import time
+from django.conf import settings
 
 class Command(BaseCommand):
     """
@@ -20,11 +22,12 @@ class Command(BaseCommand):
     args = '<corpus_filename> [...]'
     help = "Import a corpus into the database."
     option_list = BaseCommand.option_list + (
-                        make_option('-d', '--dataset',
-                            action='store',
-                            dest='dataset',
-                            help='Set a target dataset to add to'),
-                        )
+        make_option('-d', '--dataset',
+                    action='store',
+                    dest='dataset',
+                    help='Set a target dataset to add to'
+        ),
+    )
 
     def handle(self, *filenames, **options):
 
@@ -39,19 +42,34 @@ class Command(BaseCommand):
             if not path.path(f).exists():
                 raise CommandError("Filename %s does not exist" % f)
 
-
+        start = time()
         dataset_obj, created = Dataset.objects.get_or_create(name=dataset, description=dataset)
         if created:
             print "Created dataset '%s' (%d)" % (dataset_obj.name, dataset_obj.id)
         else:
             print "Adding to existing dataset '%s' (%d)" % (dataset_obj.name, dataset_obj.id)
 
-        for corpus_filename in filenames:
+        for i, corpus_filename in enumerate(filenames):
             with open(corpus_filename, 'rb') as fp:
+                if len(filenames) > 1:
+                    print "Reading file %d of %d %s" % (i + 1, len(filenames), corpus_filename)
+                else:
+                    print "Reading file %s" % corpus_filename
+
                 importer = Importer(fp, dataset_obj)
                 importer.run()
 
-        print "Dataset '%s' (%d) contains %d messages" % (dataset_obj.name, dataset_obj.id, dataset_obj.message_set.count())
+            if settings.DEBUG:
+                # prevent memory leaks
+                from django.db import connection
+                connection.queries = []
+
+        print "Dataset '%s' (%d) contains %d messages" % (
+            dataset_obj.name, dataset_obj.id, dataset_obj.message_set.count()
+        )
+        
+        print "Time: %.2fs" % (time() - start)
+
 
 class Importer(object):
     commit_every = 100
@@ -66,7 +84,7 @@ class Importer(object):
         self.errors = 0
 
     def _import_group(self, lines):
-        with transaction.atomic():
+        with transaction.atomic(savepoint=False):
             for json_str in lines:
 
                 if len(json_str) > 0:
@@ -81,7 +99,6 @@ class Importer(object):
                         traceback.print_exc()
 
     def run(self):
-        from time import time
         transaction_group = []
 
         start = time()
@@ -96,9 +113,11 @@ class Importer(object):
                 transaction_group = []
 
             if self.line > 0 and self.line % self.print_every == 0:
-                print "%6.2fs | Reached line %d. Imported: %d; Non-tweets: %d; Errors: %d" % (time() - start, self.line, self.imported, self.not_tweets, self.errors)
+                print "%6.2fs | Reached line %d. Imported: %d; Non-tweets: %d; Errors: %d" % (
+                time() - start, self.line, self.imported, self.not_tweets, self.errors)
 
         if len(transaction_group) >= 0:
             self._import_group(transaction_group)
 
-        print "%6.2fs | Finished %d lines. Imported: %d; Non-tweets: %d; Errors: %d" % (time() - start, self.line, self.imported, self.not_tweets, self.errors)
+        print "%6.2fs | Finished %d lines. Imported: %d; Non-tweets: %d; Errors: %d" % (
+        time() - start, self.line, self.imported, self.not_tweets, self.errors)
