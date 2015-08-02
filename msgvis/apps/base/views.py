ebf1fdcaa1c3cc29939ca585ac8e37001766b99e
nanchenchen <nanchen.chen@gmail.com>
2015-08-01 21:24:16 -0700

    adding new view for new ui

--- msgvis/apps/base/views.py
+++ msgvis/apps/base/views.py
@@ -41,3 +41,33 @@ class ExplorerView(generic.DetailView):
                           {'verbose_name': queryset.model._meta.verbose_name})
         return obj
 
+class GrouperView(generic.DetailView):
+    """The view for the visualization tool."""
+
+    template_name = 'grouper.html'
+
+    pk_url_kwarg = 'dataset_pk'
+    default_dataset_pk = 1
+
+    def get_queryset(self):
+        return corpus_models.Dataset.objects.all()
+
+
+    def get_object(self, queryset=None):
+
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        pk = self.kwargs.get(self.pk_url_kwarg, None)
+        if pk is None:
+            pk = self.default_dataset_pk
+
+        queryset = queryset.filter(pk=pk)
+
+        try:
+            # Get the single item from the filtered queryset
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(_("No %(verbose_name)s found matching the query") %
+                          {'verbose_name': queryset.model._meta.verbose_name})
+        return obj
