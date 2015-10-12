858ab2fe51d20aa9432b3454c3796b9a77dd0e1b
nanchenchen <nanchen.chen@gmail.com>
2015-10-12 11:18:48 -0700

    rewrite precalc command

--- msgvis/apps/enhance/management/commands/precalc_categorical_distribution.py
+++ msgvis/apps/enhance/management/commands/precalc_categorical_distribution.py
@@ -4,10 +4,9 @@ from django.db import transaction
 
 class Command(BaseCommand):
     help = "Extract topics for a dataset."
-    args = "<dataset id>"
+    args = "<dataset id> [categorical_dimensions...]"
 
-
-    def handle(self, dataset_id, *args, **options):
+    def handle(self, dataset_id, *dimensions, **options):
 
         if not dataset_id:
             raise CommandError("Dataset id is required.")
@@ -17,8 +16,13 @@ class Command(BaseCommand):
             raise CommandError("Dataset id must be a number.")
 
         from msgvis.apps.enhance.tasks import precalc_categorical_dimension
-        #categorical_dimensions = ["hashtags", "words", "urls", "timezone", "contains_media", "sentiment", "type", "sender", "mentions"]
-        categorical_dimensions = ["hashtags", "urls", "timezone", "contains_media", "sentiment", "type", "sender", "mentions"]
+
+        categorical_dimensions = []
+        if len(dimensions) == 0:
+            categorical_dimensions = ["hashtags", "words", "urls", "timezone", "contains_media", "sentiment", "type", "sender", "mentions"]
+        else:
+            categorical_dimensions = dimensions
+        #categorical_dimensions = ["hashtags", "urls", "timezone", "contains_media", "sentiment", "type", "sender", "mentions"]
         #categorical_dimensions = ["words"]
         for dimension_key in categorical_dimensions:
             print >>sys.stderr, "Precalculating %s..." %(dimension_key)
