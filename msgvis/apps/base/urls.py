307d1bd5daaff51c273b4d859bfb280f967d72ec
nanchenchen <nanchen.chen@gmail.com>
2016-08-30 08:10:44 -0700

    make it anomymous

--- msgvis/apps/base/urls.py
+++ msgvis/apps/base/urls.py
@@ -6,8 +6,9 @@ from msgvis.apps.base import views
 urlpatterns = patterns('',
                        url(r'^$', views.HomeView.as_view(), name='home'),
                        # url(r'^explorer/$', views.ExplorerView.as_view(), name='explorer'),
-                       (r'^accounts/logout/$', 'django.contrib.auth.views.logout',{'next_page': '/'}),
+                       (r'^accounts/logout/$', 'django.contrib.auth.views.logout',{'next_page': '/lariat'}),
                        url(r'^accounts/', include('django.contrib.auth.urls')),
-                       url(r'^explorer(?:/(?P<dataset_pk>\d+))?/$', views.ExplorerView.as_view(), name='explorer'),
-                       url(r'^grouper(?:/(?P<dataset_pk>\d+))?/$', views.GrouperView.as_view(), name='grouper'),
+                       #url(r'^explorer(?:/(?P<dataset_pk>\d+))?/$', views.ExplorerView.as_view(), name='explorer'),
+                       #url(r'^grouper(?:/(?P<dataset_pk>\d+))?/$', views.GrouperView.as_view(), name='grouper'),
+                       url(r'^lariat(?:/(?P<dataset_pk>\d+))?/$', views.GrouperView.as_view(), name='lariat'),
 )
